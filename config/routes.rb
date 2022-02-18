cd691e09c8e766f73e7672ee891da21c2b6ea7e8
julien kiang <kiang.jn@gmail.com>
2022-02-18 16:31:10 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # get 'bookmarks/new'
-  # get 'bookmarks/create'
-  # get 'bookmarks/destroy'
-  # get 'lists/index'
-  # get 'lists/show'
-  # get 'lists/new'
-  # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "lists#index"
   resources :lists, only: [:index, :show, :new, :create] do
